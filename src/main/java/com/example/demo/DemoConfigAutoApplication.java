81803b9830952ebda75526e5f64597b8f3e386b8
Gowtham Maddi <mmaddi1@paypal.com>
2018-01-17 18:21:35 +0530

    Added autowiring snippet

--- src/main/java/com/example/demo/DemoConfigAutoApplication.java
+++ src/main/java/com/example/demo/DemoConfigAutoApplication.java
@@ -2,11 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class DemoConfigAutoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoConfigAutoApplication.class, args);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(com.example.demo.SgtPeppers.class);
+		SgtPeppers pepper = context.getBean(SgtPeppers.class);
+		pepper.play();
+		
+		context.close();
 	}
 }
